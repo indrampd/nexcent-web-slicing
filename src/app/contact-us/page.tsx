1756c303e4818e5a496f7122bbb57ff0223ae5fd
indrampd <indrampd21@gmail.com>
2024-05-22 20:59:19 +0700

    add new page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>
+        Contact Us
+      </h1>
+    </main>
+  );
+}
---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>
+        Testimonials
+      </h1>
+    </main>
+  );
+}
